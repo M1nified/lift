11ee082774128fb8a84f2e61c1b266de9f318571
Minified <gora.pub@gmail.com>
2016-10-13 21:16:12 +0200

    remove javascrip dir

--- javascript/Floor.d.ts
+++
@@ -1,6 +0,0 @@
-declare class Floor {
-    private number;
-    constructor(number: number);
-    is(floorNumberToCompareWith: any): boolean;
-    isInBounds(min: any, max: any): boolean;
-}
--- javascript/LiftDriver.d.ts
+++
@@ -1,7 +0,0 @@
-declare class LiftDriver {
-    private tasks;
-    private floorMin;
-    private floorMax;
-    constructor(floorMin: number, floorMax: number);
-    outerButtonAction(info: OuterButtonAction, event?: Object): void;
-}
